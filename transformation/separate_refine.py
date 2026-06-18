f0e72bc15af426276fbd932099f23c47b44ffbe4
박소연 <soyeon6527@gmail.com>
2026-06-18 15:33:00 +0900

    feat: 재련 수치 분리

---
+++ transformation/separate_refine.py
@@ -0,0 +1,20 @@
+import json
+
+with open("json/transform/weapon.json", "r", encoding="utf-8") as f:
+    weapons = json.load(f)
+
+for weapon in weapons:
+    refine_value = weapon.get("refine_value")
+
+    if refine_value:
+        values = refine_value.split("/")
+
+        for i, value in enumerate(values, start=1):
+            weapon[f"refine_{i}_value"] = value
+
+        del weapon["refine_value"]
+
+with open("json/transform/weapon.json", "w", encoding="utf-8") as f:
+    json.dump(weapons, f, ensure_ascii=False, indent=4)
+
+print("완료")
