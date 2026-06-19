439372eb8cf29e009aec2cb4505bd3a154dcd1bc
박소연 <soyeon6527@gmail.com>
2026-06-19 13:04:27 +0900

    fix: 테이블명 오타 수정

---
+++ resources/sql/resonance_node_master.sql
@@ -0,0 +1,159 @@
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('치료 효과 보너스', 4.20, 1.80, 'HP', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '설지'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('응결 피해 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '산화'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('응결 피해 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '능양'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '절지'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '유호'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '카를로타'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '히유키'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '루실라'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('용융 피해 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '치샤'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('용융 피해 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '모르테피'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('용융 피해 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '앙코'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬 확률', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '장리'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '브렌트'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '루파'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬 피해', 5.60, 2.40, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '갈브레나'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('치료 효과 보너스', 4.20, 1.80, '방어력', 5.32, 2.28, (SELECT id FROM resonator_master WHERE name = '모니에'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크티리컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '에이메스'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬 피해', 5.60, 2.40, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '데니아'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('전도 피해 보너스', 4.20, 1.80, '방어력', 5.32, 2.28, (SELECT id FROM resonator_master WHERE name = '연무'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬 피해', 5.60, 2.40, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '카카루'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬 확률', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '음림'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬 피해', 5.60, 2.40, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '상리요'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '루미'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '아우구스타'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('치료 효과 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '복링'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '레베카'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('치료 효과 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '방랑자·기류'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('기류 피해 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '양양'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('기류 피해 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '알토'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '감심'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '기염'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬 피해', 5.60, 2.40, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '샤콘'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, 'HP', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '카르티시아'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '유노'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크티리컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '구원'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '시그리카'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('회절 피해 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '방랑자·회절'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('치료 효과 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '벨리나'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '금희'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('치료 효과 보너스', 4.20, 1.80, 'HP', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '파수인'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬 피해', 5.60, 2.40, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '페비'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '젠니'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '린네'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '루크·헤르센'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '루시'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('인멸 피해 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '방랑자·인멸'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('인멸 피해 보너스', 4.20, 1.80, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '단근'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('인멸 피해 보너스', 4.20, 1.80, '방어력', 5.32, 2.28, (SELECT id FROM resonator_master WHERE name = '도기'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬 피해', 5.60, 2.40, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '카멜리아'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬 피해', 5.60, 2.40, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '로코코'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '칸타렐라'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '플로로'));
+
+INSERT INTO resonance_node_master (outer_node_type, outer_top_node_value, outer_middle_node_value, inner_node_type, inner_top_node_value, inner_middle_node_value, resonator_master_id)
+VALUES ('크리티컬', 2.80, 1.20, '공격력', 4.20, 1.80, (SELECT id FROM resonator_master WHERE name = '치사'));
+
